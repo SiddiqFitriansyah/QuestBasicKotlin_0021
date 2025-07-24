c038bc853b31641fbf1f431c301733e8f4255161
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2025-07-24 18:58:19 +0700

    Add contoh penggunaan list

--- Activity2.kt
+++ Activity2.kt
@@ -17,5 +17,34 @@ fun main() {
     // Prints the new value of the property: email
     println(contact.email)
 
+    // www.dicoding.com
+
+/*
+ * List
+ * List adalah collection yang bersifat immutable, artinya elemennya tidak bisa diubah setelah List dibuat.
+ * List juga mempertahankan urutan elemen yang dimasukkan.
+ * Untuk membuat List, kita bisa menggunakan fungsi listOf() atau mutableListOf() jika kita ingin List yang bisa diubah.
+ */
+
+// Contoh penggunaan List
+val numberList = listOf(1, 2, 3, 4, 5)
+println(numberList)
+println(numberList[0]) // Mengakses elemen pertama
+
+val anyList = listOf("Circle", "Square", "Triangle", 1, 2, 3, true, false)
+println(anyList)
+
+val mutableList = mutableListOf("Circle", "Square", "Triangle")
+mutableList.add("Rectangle") // Menambah elemen baru
+mutableList.removeAt(1) // Menghapus elemen pada indeks 1 (Square)
+mutableList[0] = "Oval" // Mengubah elemen pada indeks 0 (Circle menjadi Oval)
+println(mutableList)
+
+/*
+ * Set
+ * Set adalah collection yang hanya menyimpan nilai-nilai unik. Jika Anda mencoba menambahkan elemen yang sudah ada, Set akan mengabaikannya.
+ * Set tidak mempertahankan urutan elemen yang dimasukkan.
+ * Untuk membuat Set, kita bisa menggunakan fungsi setOf() atau mutableSetOf().
+ */
 
 }
