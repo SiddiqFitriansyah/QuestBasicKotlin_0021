d0c074d6fa24d7ccea3255dd30b40d2ce96fe887
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2025-07-24 18:49:17 +0700

    add Prints the value of the property: email

--- Activity2.kt
+++ Activity2.kt
@@ -3,3 +3,12 @@ package com.example.test
 class Motor ()
 
 class Contact(val id: Int, var email: String)
+
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    // Prints the value of the property: email
+    println(contact.email)
+
+}
